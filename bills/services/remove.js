6de11697a6ede7267d58b246a409612605a8bc66
Leonardo Cardoso <leocardosodev@gmail.com>
2020-07-20 14:07:27 -0300

    feat: add delete

---
+++ bills/services/remove.js
@@ -0,0 +1,11 @@
+import Bill from './../schema/Bill'
+
+export default (req, res) => {
+  Bill
+  .findByIdAndRemove(req.params.id)
+  .then(() => res.status(204).end())
+  .catch(error => res.status(500).json({
+    status: false,
+    data: {}
+  }))
+}
